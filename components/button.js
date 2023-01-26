b6eddecab8eee41ca8e8358c5af3ef9b334512cb
Yannick LEFAIVRE <yannick.lefaivre@outlook.com>
2023-01-26 19:59:10 -0400

    work in progress on branch button

---
+++ components/button.js
@@ -0,0 +1,42 @@
+const template = document.createElement("template");
+
+template.innerHTML = `
+<style>
+.button {
+  display: flex;
+  flex-direction: row;
+  column-gap: 15px;
+  padding: 0.75em 1.125em; /* 0.75em = 12px et 1.125em = 18px */
+  border: transparent;
+  border-radius: 6px;
+  background-color: rgba(69, 87, 122, 1);
+  color: #fff;
+}
+
+.button-icon {
+  font-size: 1.375rem; /* 22px / body font-size (16px) */
+}
+
+.button-text {
+  font-size: 1.375rem; /* 22px / body font-size (16px) */
+}
+</style>
+
+<button class="button">
+</button>
+`;
+
+class Button extends HTMLElement{
+  constructor() {
+    super(); 
+    this.attachShadow({ mode: 'open' });
+    this.shadowRoot.appendChild(template.content.cloneNode(true));
+    
+    this.button = this.shadowRoot.querySelector('.button')
+
+    this.button.innerHTML = this.getAttribute('label')
+    this.button.setAttribute('type', this.getAttribute('type'))
+  }
+}
+
+customElements.define("button-component", Button);
